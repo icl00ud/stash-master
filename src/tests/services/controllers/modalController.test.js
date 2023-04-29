90312db234e78d4565ce841183ab3e69128c73ce
Israel Schroeder <israelschroederm@gmail.com>
2023-04-29 01:19:08 -0300

    Fixed modal controller tests

--- src/tests/services/controllers/modalController.test.js
+++ src/tests/services/controllers/modalController.test.js
@@ -2,19 +2,19 @@ const request = require("supertest");
 const app = require("../../../services/server/server");
 
 describe("Stock Route Tests", () => {
-  it("deve retornar código 200 e tipo text/html da modal de create_product", async () => {
+  it("should serve create product modal", async () => {
     const res = await request(app).get("/stock/create_product");
     expect(res.statusCode).toEqual(200);
     expect(res.type).toEqual("text/html");
   });
 
-  it("deve retornar código 200 e tipo text/html da modal de update_product", async () => {
+  it("should serve update product modal", async () => {
     const res = await request(app).get("/stock/update_product");
     expect(res.statusCode).toEqual(200);
     expect(res.type).toEqual("text/html");
   });
 
-  it("deve retornar código 200 e tipo text/html da modal de delete_product", async () => {
+  it("should serve delete product modal", async () => {
     const res = await request(app).get("/stock/delete_product");
     expect(res.statusCode).toEqual(200);
     expect(res.type).toEqual("text/html");
