39338580a2e960fa883ff2bc6e6089633f705924
Israel Schroeder <israelschroederm@gmail.com>
2023-05-24 00:25:34 -0300

    Added tabs to panel page

--- src/pages/panel/panel.js
+++ src/pages/panel/panel.js
@@ -1,5 +1,25 @@
 var stockHeaderButton = document.getElementById("stock");
 var panelHeaderButton = document.getElementById("panel");
+var userTab = document.getElementById("user-tab");
+var providerTab = document.getElementById("provider-tab");
+var userContent = document.getElementById("user-content");
+var providerContent = document.getElementById("provider-content");
+
+userTab.addEventListener("click", () => {
+  userTab.classList.add("active");
+  providerTab.classList.remove("active");
+
+  userContent.style.display = "block";
+  providerContent.style.display = "none";
+});
+
+providerTab.addEventListener("click", () => {
+  userTab.classList.remove("active");
+  providerTab.classList.add("active");
+
+  userContent.style.display = "none";
+  providerContent.style.display = "block";
+});
 
 stockHeaderButton.addEventListener("click", (event) => {
   var urlAtual = window.location.href;
